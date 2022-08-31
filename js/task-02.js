5171b45db77c1d7414e64d13d94e50d90ab4bb51
Dmytro Vorobets <dmytrovorobets025@gmail.com>
2022-08-31 18:49:44 +0300

    1-3

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector('#ingredients')
+const listIngredients = ingredients.map((ingredient) => {
+  const listItem = document.createElement('li');
+  listItem.textContent = ingredient;
+  listItem.classList.add('item');
+  return listItem;
+})
+ingredientsEl.append(...listIngredients);
+
