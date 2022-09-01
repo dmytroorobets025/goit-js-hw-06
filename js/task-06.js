b71329442a405800a1bcfd411a79e2ca066ce6a3
Dmytro Vorobets <dmytrovorobets025@gmail.com>
2022-09-01 20:45:28 +0300

    -6

--- js/task-06.js
+++ js/task-06.js
@@ -9,6 +9,8 @@ refs.inputRef.addEventListener("blur", (event) => {
     if (inputText.length === +refs.inputRef.dataset.length) {
         refs.inputRef.classList.remove("invalid");
         return refs.inputRef.classList.add("valid");
+    } else {
+        refs.inputRef.classList.remove("valid");
+        return refs.inputRef.classList.add("invalid");
     }
-    return refs.inputRef.classList.add("invalid");
 });
